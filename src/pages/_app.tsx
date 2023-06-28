23df67bf8f93d900306c83f89b635fb7b6d17a28
seokhwalee <smmc05004@hanmail.net>
2023-06-28 17:01:45 +0900

    fix: 불필요한 파일 및 주석 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,17 @@
-// import '@/styles/globals.css'
-// import type { AppProps } from 'next/app'
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
-
 import '../styles/globals.css';
 import GlobalStyle from '@components/common/GlobalStyle';
 import Layout from '@components/common/layout';
 import type { AppProps } from 'next/app';
-
-// import CommonLayout from '@components/layout';
 // import { DefaultTheme, ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
+
       <Layout>
         {/* <ThemeProvider theme={theme}> */}
-        {/* <CommonLayout> */}
         <Component {...pageProps} />
-        {/* </CommonLayout> */}
         {/* </ThemeProvider> */}
       </Layout>
     </>
